d50ecee979f968adcc92185849e00186cedd4d1c
Kostadin Slavov <kostadin.slavov@tixi.bg>
2023-06-05 13:13:30 +0300

    Do not edit/crop the picture

--- components/UserInterfacePage.js
+++ components/UserInterfacePage.js
@@ -53,7 +53,7 @@ const UserInterfacePage = ({navigation}) => {
     const pickImage = async () => {
         let result = await ImagePicker.launchImageLibraryAsync({
             mediaTypes: ImagePicker.MediaTypeOptions.All,
-            allowsEditing: true,
+            allowsEditing: false,
             // aspect: [4, 3],
             quality: 1,
         });
